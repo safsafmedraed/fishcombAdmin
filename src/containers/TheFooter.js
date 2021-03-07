e753ac0e59a62e2be9f18a752a418f72a357aab2
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-03-07 21:39:48 +0100

    add_folders_to_actions_reducers

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -5,12 +5,12 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">FishComb</a>
+        <a href="#" target="_blank" rel="noopener noreferrer">FishComb</a>
         <span className="ml-1">&copy; 2021 </span>
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">FishComb</a>
+        <a href="#" target="_blank" rel="noopener noreferrer">FishComb</a>
       </div>
     </CFooter>
   )
